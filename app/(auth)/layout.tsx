d48a79f791c03edbbf9c7da4618fad4b3d2033e5
Miguel <migui.kaos14@gmail.com>
2026-02-19 11:32:21 +0100

    feat: redirect if we are already logged in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,14 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 
-export default function AuthLayout({ children }: PropsWithChildren) {
+export default async function AuthLayout({ children }: PropsWithChildren) {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="flex flex-row justify-center align-middle items-center w-full h-screen">
       {children}

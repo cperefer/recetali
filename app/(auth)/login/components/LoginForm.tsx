6c8dc4d79e1f82aef2addef86b2a6c7ceb76e351
Miguel <migui.kaos14@gmail.com>
2026-02-19 14:47:16 +0100

    feat: changed styles for login

--- app/(auth)/login/components/LoginForm.tsx
+++ app/(auth)/login/components/LoginForm.tsx
@@ -16,20 +16,20 @@ export default function LoginForm() {
       <form action={formAction} className="h-full flex flex-col justify-around">
         <div className="h-4/6 bg-[url(/login.webp)] bg-cover rounded-2xl rounded-b-[200px] "></div>
         <div className="h-3/6 flex flex-col justify-around">
-          <div className="flex flex-col items-center">
-            <input
-              className={`border-2 border-gray-400 rounded-sm w-3/5 pl-1 ${state?.errors?.email && "border-red-400"}`}
-              type="text"
-              name="email"
-              id="email"
-              defaultValue={state?.email}
-              placeholder="Email"
-            />
-          </div>
-          <div>
+          <div className="h-3/4 flex flex-col justify-around">
             <div className="flex flex-col items-center">
               <input
-                className={`border-2 border-gray-400 rounded-sm w-3/5 pl-1 ${state?.errors?.password && "border-red-400"}`}
+                className={`border-2 border-gray-400 rounded-sm w-3/5 pl-1 ${state?.error && "border-red-400"}`}
+                type="text"
+                name="email"
+                id="email"
+                defaultValue={state?.email}
+                placeholder="Email"
+              />
+            </div>
+            <div className="flex flex-col items-center">
+              <input
+                className={`border-2 border-gray-400 rounded-sm w-3/5 pl-1 ${state?.error && "border-red-400"}`}
                 type="password"
                 name="password"
                 id="password"
@@ -37,13 +37,18 @@ export default function LoginForm() {
                 placeholder="Password"
               />
             </div>
-            <div className="text-right hidden pr-2 pt-2">
-              <p className="text-xs md:text-sm">
-                ¿Has olvidado tu contraseña? Haz click aquí
+            <div className="flex flex-col items-center">
+              <p className="text-red-400 text-xs md:text-sm">
+                {state?.error || ""}
               </p>
             </div>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="h-1/2 flex flex-col items-center">
+            <div className="h-2/4 text-center pr-2 pt-2">
+              <p className="text-xs hidden md:text-sm">
+                ¿Has olvidado tu contraseña? Haz click aquí
+              </p>
+            </div>
             <button
               disabled={isPending}
               className={`cursor-pointer border-2  w-25 h-7.5 rounded-2xl text-black ${isPending ? "bg-gray-400 border-gray-400" : "bg-white border-white"} `}

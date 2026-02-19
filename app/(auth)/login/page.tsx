bb9f31a399143157b2bc9df63a6eeba0071f2c61
Miguel <migui.kaos14@gmail.com>
2026-02-19 09:03:31 +0100

    feat: added image for login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -14,8 +14,8 @@ export default function LoginPage() {
   return (
     <div className="h-3/5 max-h-150 md:h-4/5 w-4/5 md:w-1/3 border-2 border-white rounded-2xl ">
       <form action={formAction} className="h-full flex flex-col justify-around">
-        <div className="h-2/5"></div>
-        <div className="h-3/5 flex flex-col justify-around">
+        <div className="h-4/6 bg-[url(/login.webp)] bg-cover rounded-2xl rounded-b-[200px] "></div>
+        <div className="h-3/6 flex flex-col justify-around">
           <div className="flex flex-col items-center">
             <input
               className={`border-2 border-gray-400 rounded-sm w-3/5 pl-1 ${state?.errors?.email && "border-red-400"}`}
@@ -37,7 +37,7 @@ export default function LoginPage() {
                 placeholder="Password"
               />
             </div>
-            <div className="text-right pr-2 pt-2">
+            <div className="text-right hidden pr-2 pt-2">
               <p className="text-xs md:text-sm">
                 ¿Has olvidado tu contraseña? Haz click aquí
               </p>

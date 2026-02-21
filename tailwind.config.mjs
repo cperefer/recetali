c9ce56ba38cfe0894286407df9ddd1a538b6eae3
Miguel <migui.kaos14@gmail.com>
2026-02-21 11:25:03 +0100

    feat: changed tailwind configuration

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default tailwindConfig = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

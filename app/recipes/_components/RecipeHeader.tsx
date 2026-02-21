065b827a0331107ac4be22a0c0893f114d6ebd9f
Miguel <migui.kaos14@gmail.com>
2026-02-21 13:36:07 +0100

    feat: changed some styles for RecipeHeader

--- app/recipes/_components/RecipeHeader.tsx
+++ app/recipes/_components/RecipeHeader.tsx
@@ -1,3 +1,5 @@
+import { fromDatabaseToHuman } from "@/utilites/parseDificulty";
+
 type Props = {
   imageUrl: string;
   title: string;
@@ -15,20 +17,15 @@ export function RecipeHeader({
 }: Props) {
   return (
     <div
-      className={`w-full h-1/3 bg-contain sm:bg-auto relative`}
+      className={`w-full h-1/3 bg-cover sm:bg-auto relative`}
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
       <div className="`w-full h-full flex flex-col justify-end items-center bg-black/20 backdrop-opacity-60">
-        <div className="text-center pb-5">
-          <h1
-            className="text-2xl md:text-6xl text-shadow-lg
-"
-          >
-            {title}
-          </h1>
+        <div className="text-center pb-5 text-shadow-lg">
+          <h1 className="text-3xl md:text-6xl">{title}</h1>
           <div className="pt-2">
             <p>
-              {time} mins · {dificulty} · {people} personas
+              {time} mins · {fromDatabaseToHuman(dificulty)} · {people} personas
             </p>
           </div>
         </div>

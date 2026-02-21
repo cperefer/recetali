949ed2cb11a87982a37866613222272c982bc97c
Miguel <migui.kaos14@gmail.com>
2026-02-21 17:58:22 +0100

    feat: added padding for badges container

--- app/recipes/_components/RecipeBadges.tsx
+++ app/recipes/_components/RecipeBadges.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function RecipeBadges({ time, dificulty, people }: Props) {
   return (
-    <div className="w-full h-1/12 py-3 flex justify-center items-center gap-2 md:gap-4 border-b border-gray-400/30">
+    <div className="w-full h-1/12 py-3 px-2 flex justify-center items-center gap-2 md:gap-4 border-b border-gray-400/30">
       <RecipeBadge text={`${String(time)} min`} type="time" />
       <RecipeBadge text={fromDatabaseToHuman(dificulty)} type="dificulty" />
       <RecipeBadge text={`${String(people)} personas`} type="pax" />

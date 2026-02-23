8741b8a18b7f6987642f168d6da09c8f78720843
Miguel <migui.kaos14@gmail.com>
2026-02-23 16:29:02 +0100

    feat: added functions to manage favorite recipes

--- lib/dal.ts
+++ lib/dal.ts
@@ -62,3 +62,68 @@ export const getRecipeIngredientsById = async (id: number) => {
     return null;
   }
 };
+
+export const getRecipeFavoriteForUser = async (
+  userId: number,
+  recipeId: number,
+) => {
+  // console.log({ id });
+
+  try {
+    const result = await prisma.favorite.findUnique({
+      where: { userId_recipeId: { userId, recipeId } },
+    });
+
+    return result || null;
+  } catch (error) {
+    console.error("Error getting favorite", error);
+
+    return null;
+  }
+};
+
+export const insertFavoriteRecipe = async (
+  userId: number,
+  recipeId: number,
+) => {
+  // console.log({ id });
+
+  try {
+    const result = await prisma.favorite.create({
+      data: {
+        userId,
+        recipeId,
+      },
+    });
+
+    return result || null;
+  } catch (error) {
+    console.error("Error getting favorite", error);
+
+    return null;
+  }
+};
+
+export const removeFavoriteRecipe = async (
+  userId: number,
+  recipeId: number,
+) => {
+  // console.log({ id });
+
+  try {
+    const result = await prisma.favorite.delete({
+      where: {
+        userId_recipeId: {
+          userId,
+          recipeId,
+        },
+      },
+    });
+
+    return result || null;
+  } catch (error) {
+    console.error("Error getting favorite", error);
+
+    return null;
+  }
+};

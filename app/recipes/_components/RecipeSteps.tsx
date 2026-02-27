2c46d66e8b8837ec4d6bd050eeec4608928a4f3f
Miguel <migui.kaos14@gmail.com>
2026-02-27 20:26:14 +0100

    feat: added recipe steps

--- app/recipes/_components/RecipeSteps.tsx
+++ app/recipes/_components/RecipeSteps.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-export function RecipeSteps({ steps }: { steps: string }) {
+import { Step } from "@/app/generated/prisma/client";
+
+export function RecipeSteps({ steps }: { steps: Step[] }) {
+  console.log(steps);
   return (
     <div className="mt-5 w-9/10 md:w-3/4 rounded-md shadow-md dark:shadow-xs dark:shadow-amber-600/30">
       <div className="px-2 pt-2 pb-2 pl-4 text-xl text-white bg-linear-to-r from-amber-600 to-amber-900 rounded-t-sm">
@@ -8,12 +11,12 @@ export function RecipeSteps({ steps }: { steps: string }) {
       </div>
       <div className="mt-1 pb-4 px-4 text-justify">
         <ul>
-          {steps.split("<br />").map((step: string, index: number) => (
-            <li key={index}>
+          {steps.map(({ step, order }: Step) => (
+            <li key={order}>
               <span className="text-amber-600 text-2xl font-bold w-8 inline-block text-right mr-1">
-                {index + 1}.
+                {order}.
               </span>
-              {step.trim()}
+              {step}
             </li>
           ))}
         </ul>

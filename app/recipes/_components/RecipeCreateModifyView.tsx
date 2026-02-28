e0bc96deaa537a770b7631428486633b998c677b
Miguel <migui.kaos14@gmail.com>
2026-02-28 11:28:52 +0100

    feat: created Recipe add and modify view

---
+++ app/recipes/_components/RecipeCreateModifyView.tsx
@@ -0,0 +1,9 @@
+import { Recipe } from "@/app/generated/prisma/client";
+
+export function RecipeCreateModifyView({ recipe }: { recipe?: Recipe }) {
+  return (
+    <div>
+      <div>jibiri jibiri</div>
+    </div>
+  );
+}

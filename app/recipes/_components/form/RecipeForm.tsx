96e0ee45e0fc3be60ef26780d72e170bf4104455
Miguel <migui.kaos14@gmail.com>
2026-02-28 11:47:04 +0100

    feat: added RecipeHeader

--- app/recipes/_components/form/RecipeForm.tsx
+++ app/recipes/_components/form/RecipeForm.tsx
@@ -1,9 +1,11 @@
 import { Recipe } from "@/app/generated/prisma/client";
+import { RecipeFormHeader } from "./RecipeFormHeader";
 
 export function RecipeForm({ recipe }: { recipe?: Recipe }) {
+  console.log(recipe);
   return (
     <div>
-      <div>jibiri jibiri</div>
+      <RecipeFormHeader />
     </div>
   );
 }
---
+++ app/recipes/_components/form/RecipeFormHeader.tsx
@@ -0,0 +1,11 @@
+export function RecipeFormHeader() {
+  return (
+    <div>
+      <div>botoncito</div>
+      <div>
+        <h1 className="">Crear una receta</h1>
+        <h2>Comparte una receta para no olvidarla!</h2>
+      </div>
+    </div>
+  );
+}

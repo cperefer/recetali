3ef74ef89c8d4aba00091873b3542d660f0171ad
Miguel <migui.kaos14@gmail.com>
2026-02-28 12:07:16 +0100

    feat: added title

--- app/recipes/add/page.tsx
+++ app/recipes/add/page.tsx
@@ -1,6 +1,12 @@
 import { validateSession } from "@/utilites/validateSession";
 import { RecipeForm } from "@recipes/_components/form/RecipeForm";
 
+export async function generateMetadata() {
+  return {
+    title: "Recetali - Crear receta",
+  };
+}
+
 export default async function AddRecipePage() {
   await validateSession();
 

e4f1c0904390f7b75f5ccf86e6cb10d30ce68394
Miguel <migui.kaos14@gmail.com>
2026-03-01 18:36:04 +0100

    feat: added information to form

--- app/recipes/_components/form/RecipeForm.tsx
+++ app/recipes/_components/form/RecipeForm.tsx
@@ -1,11 +1,13 @@
 import { Recipe } from "@/app/generated/prisma/client";
 import { RecipeFormHeader } from "./RecipeFormHeader";
+import { RecipeFormInformation } from "./RecipeFormInformation";
 
 export function RecipeForm({ recipe }: { recipe?: Recipe }) {
   console.log(recipe);
   return (
     <div className="w-full h-full px-3 md:px-5 pt-5 flex flex-col relative">
       <RecipeFormHeader />
+      <RecipeFormInformation />
     </div>
   );
 }

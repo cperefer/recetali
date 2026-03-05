be53453f2c642c6957aeb2924f2c84c0fcd938e1
Miguel <migui.kaos14@gmail.com>
2026-03-05 09:47:11 +0100

    feat: added background for light mode

--- app/recipes/_components/form/RecipeForm.tsx
+++ app/recipes/_components/form/RecipeForm.tsx
@@ -5,7 +5,7 @@ import { RecipeFormInformation } from "./RecipeFormInformation";
 export function RecipeForm({ recipe }: { recipe?: Recipe }) {
   console.log(recipe);
   return (
-    <div className="w-full h-full px-3 md:px-5 pt-5 flex flex-col relative">
+    <div className="w-full h-full px-3 md:px-5 pt-5 flex flex-col relative bg-gray-100 dark:bg-transparent">
       <RecipeFormHeader />
       <RecipeFormInformation />
     </div>

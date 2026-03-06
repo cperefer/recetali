c316ab14ad9c71ffe4d627ee35a8b6731d5492ed
Miguel <migui.kaos14@gmail.com>
2026-03-06 09:05:26 +0100

    feat: added responsive classes

--- app/recipes/_components/form/RecipeForm.tsx
+++ app/recipes/_components/form/RecipeForm.tsx
@@ -1,13 +1,21 @@
 import { Recipe } from "@/app/generated/prisma/client";
 import { RecipeFormHeader } from "./RecipeFormHeader";
 import { RecipeFormInformation } from "./RecipeFormInformation";
+import { RecipeFormImage } from "./RecipeFormImage";
 
 export function RecipeForm({ recipe }: { recipe?: Recipe }) {
   console.log(recipe);
   return (
     <div className="w-full h-full px-3 md:px-5 pt-5 flex flex-col relative bg-gray-100 dark:bg-transparent">
       <RecipeFormHeader />
-      <RecipeFormInformation />
+      <div className="flex flex-col md:flex-row gap-4 w-full mt-2 md:mt-5">
+        <div className="w-full md:w-2/3">
+          <RecipeFormInformation />
+        </div>
+        <div className="w-full md:w-1/3">
+          <RecipeFormImage />
+        </div>
+      </div>
     </div>
   );
 }

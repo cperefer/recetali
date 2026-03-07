9a17928170d7b95700124367f5cb6281e7845118
Miguel <migui.kaos14@gmail.com>
2026-03-07 10:10:32 +0100

    feat: modified recipe image component

--- app/recipes/_components/form/RecipeFormImage.tsx
+++ app/recipes/_components/form/RecipeFormImage.tsx
@@ -4,8 +4,17 @@ export function RecipeFormImage() {
       <div className="mx-2 md:mx-3 pb-1 mt-2 border-bottom-gray">
         <h4 className="text-xl font-bold">Imagen</h4>
       </div>
-      <div className="mx-2 md:mx-3">imagen</div>
-      <div className="mx-2 md:mx-3">botones</div>
+      <div className="mx-2 md:mx-3 h-30 border-2 border-dashed border-gray-400 flex items-center justify-center bg-gray-500/70">
+        <p className="text-sm">
+          Arrastra una imagen aquí o haz click para seleccionar
+        </p>
+      </div>
+      <div className="py-2 mx-2 md:mx-3 flex justify-around">
+        <button className="btn btn-primary bg-primary text-black rounded-md!">
+          Cambiar imagen
+        </button>
+        <button className="btn btn-secondary rounded-md!">Eliminar</button>
+      </div>
     </div>
   );
 }

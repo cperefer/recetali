e338a620aaec460821c177b76b04447d8890f8c9
Miguel <migui.kaos14@gmail.com>
2026-03-13 19:07:29 +0100

    feat: adde min height to recipe image

--- app/recipes/_components/form/RecipeFormImage.tsx
+++ app/recipes/_components/form/RecipeFormImage.tsx
@@ -4,7 +4,7 @@ export function RecipeFormImage() {
       <div className="mx-2 md:mx-3 pb-1 mt-2 border-bottom-gray">
         <h4 className="text-xl font-bold">Imagen</h4>
       </div>
-      <div className="mx-2 md:mx-3 mt-2 h-[60%] border-2 border-dashed border-gray-400 flex items-center justify-center bg-gray-500/70">
+      <div className="mx-2 md:mx-3 mt-2 h-[60%] min-h-40 border-2 border-dashed border-gray-400 flex items-center justify-center bg-gray-500/70">
         <p className="text-sm">
           Arrastra una imagen aquí o haz click para seleccionar
         </p>

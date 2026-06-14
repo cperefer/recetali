89cbba26671e31517dd5ba6178c41a4f11bcbf85
Miguel <migui.kaos14@gmail.com>
2026-06-14 19:34:26 +0200

    feat: added back router functionality

--- app/recipes/_components/shared/RecipeGoBackButton.tsx
+++ app/recipes/_components/shared/RecipeGoBackButton.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export function RecipeGoBackButton() {
+  const router = useRouter();
+
   return (
-    <button className="btn btn-primary btn-with-icon">
+    <button className="btn btn-primary btn-with-icon" onClick={router.back}>
       <ArrowLeft size={18} /> Volver
     </button>
   );

8a1c022c0689761664e7322575a2a3af6aa78658
Miguel <migui.kaos14@gmail.com>
2026-06-25 12:36:28 +0200

    feat: added form controls for information

--- app/recipes/_components/form/RecipeForm.tsx
+++ app/recipes/_components/form/RecipeForm.tsx
@@ -1,31 +1,48 @@
+"use client";
+
 import { Recipe } from "@/app/generated/prisma/client";
 import { RecipeFormHeader } from "./RecipeFormHeader";
 import { RecipeFormInformation } from "./RecipeFormInformation";
 import { RecipeFormImage } from "./RecipeFormImage";
 import { RecipeFormIngredients } from "./RecipeFormIngredients";
 import { RecipeFormSteps } from "./RecipeFormSteps";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+
+interface FormValues {
+  title?: string;
+  description?: string;
+  ingredients?: string[];
+}
 
 export function RecipeForm({ recipe }: { recipe?: Recipe }) {
   console.log(recipe);
+  const methods = useForm();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
   return (
-    <div className="w-full h-full px-3 md:px-5 pt-5 flex flex-col relative bg-gray-100 dark:bg-transparent">
-      <RecipeFormHeader />
-      <div className="create-recipe-section">
-        <div className="w-full md:w-2/3">
-          <RecipeFormInformation />
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <div className="w-full h-full px-3 md:px-5 pt-5 flex flex-col relative bg-gray-100 dark:bg-transparent">
+          <RecipeFormHeader />
+          <div className="create-recipe-section">
+            <div className="w-full md:w-2/3">
+              <RecipeFormInformation />
+            </div>
+            <div className="w-full md:w-1/3">
+              <RecipeFormImage />
+            </div>
+          </div>
+          <div className="create-recipe-section">
+            <div className="w-full md:w-2/3">
+              <RecipeFormSteps />
+            </div>
+            <div className="w-full md:w-1/3">
+              <RecipeFormIngredients />
+            </div>
+          </div>
         </div>
-        <div className="w-full md:w-1/3">
-          <RecipeFormImage />
-        </div>
-      </div>
-      <div className="create-recipe-section">
-        <div className="w-full md:w-2/3">
-          <RecipeFormSteps />
-        </div>
-        <div className="w-full md:w-1/3">
-          <RecipeFormIngredients />
-        </div>
-      </div>
-    </div>
+        <button>manda cosass</button>
+      </form>
+    </FormProvider>
   );
 }
--- app/recipes/_components/form/RecipeFormInformation.tsx
+++ app/recipes/_components/form/RecipeFormInformation.tsx
@@ -1,25 +1,29 @@
+import { useFormContext } from "react-hook-form";
+
 export function RecipeFormInformation() {
+  const { register } = useFormContext();
   return (
     <div className="px-2 border bg-white dark:bg-transparent rounded-xl min-h-67">
       <div className="mx-2 md:mx-3 pb-1 mt-2 border-bottom-gray">
         <h4 className="text-xl font-bold">Información básica</h4>
       </div>
       <div className="mt-2 px-2 md:px-3">
-        <form>
-          <div className="flex flex-col">
-            <label htmlFor="title">Título</label>
-            <input
-              type="text"
-              className="pl-1 h-8 border border-primary rounded-md"
-              name="title"
-              id="title"
-            />
-          </div>
-          <div className="flex flex-col mt-1">
-            <label htmlFor="">Descripción</label>
-            <textarea className="pl-1 h-15 border border-gray-300 dark:border-primary rounded-md"></textarea>
-          </div>
-        </form>
+        <div className="flex flex-col">
+          <label htmlFor="title">Título</label>
+          <input
+            type="text"
+            className="pl-1 h-8 border border-primary rounded-md"
+            id="title"
+            {...register("title", { required: true })}
+          />
+        </div>
+        <div className="flex flex-col mt-1">
+          <label htmlFor="">Descripción</label>
+          <textarea
+            {...register("description", { required: true })}
+            className="pl-1 h-15 border border-gray-300 dark:border-primary rounded-md"
+          ></textarea>
+        </div>
       </div>
       <div className="px-2 md:px-3 py-5 flex justify-around">
         <div className="w-1/3">
@@ -27,8 +31,8 @@ export function RecipeFormInformation() {
           <input
             type="text"
             className="ml-1 pl-1 w-20 h-8 border border-primary rounded-md"
-            name="people"
             id="people"
+            {...register("people", { required: true })}
           />
         </div>
         <div className="w-1/3">
@@ -36,17 +40,17 @@ export function RecipeFormInformation() {
           <input
             type="text"
             className="ml-1 pl-1 w-20 h-8 border border-primary rounded-md "
-            name="time"
             id="time"
             placeholder="min."
+            {...register("time", { required: true })}
           />
         </div>
         <div className="w-1/3">
           <label htmlFor="dificulty">Dificultad</label>
           <select
             className="ml-1 pl-1 w-20 h-8 border border-primary rounded-md"
-            name="dificulty"
             id="dificulty"
+            {...register("dificulty", { required: true })}
           >
             <option className="bg-black" value="easy">
               Fácil

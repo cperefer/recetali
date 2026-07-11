291681eea2ffcddecd02259ad6f54df83e4d7eec
Miguel <migui.kaos14@gmail.com>
2026-07-11 09:27:28 +0200

    feat: created action to create recipe and redirect to its page

--- app/recipes/_components/form/RecipeForm.tsx
+++ app/recipes/_components/form/RecipeForm.tsx
@@ -7,25 +7,38 @@ import { RecipeFormHeader } from "./RecipeFormHeader";
 import { RecipeFormInformation } from "./RecipeFormInformation";
 import { RecipeFormImage } from "./RecipeFormImage";
 import { RecipeFormRepiterContainer } from "./shared/RecipeFormRepeaterContainer";
+import { redirect } from "next/navigation";
 
 interface FormValues {
-  title?: string;
+  name?: string;
   description?: string;
+  pax?: string;
+  dificulty?: string;
+  category?: string;
+  observations?: string;
+  timeToDone?: string;
+  steps?: string[];
   ingredients?: string[];
+  image?: File;
 }
 
 export function RecipeForm({ recipe }: { recipe?: Recipe }) {
   const methods = useForm();
 
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    console.log(data);
-    createRecipe(data as Partial<Recipe>);
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    const createdRecipe: Recipe = await createRecipe(
+      data as Partial<Recipe> & { image?: File },
+    );
+
+    if (createdRecipe) {
+      redirect(`/recipes/${createdRecipe.slug}`);
+    }
   };
 
   return (
     <FormProvider {...methods}>
       <form
-        className="w-[95%] md:w-[90%] max-w-6xl h-fit"
+        className="pb-5 w-[95%] md:w-[90%] max-w-6xl h-fit"
         onSubmit={methods.handleSubmit(onSubmit)}
       >
         <div className="w-full h-full px-3 md:px-5 pt-5 flex flex-col relative bg-gray-100 dark:bg-transparent">
@@ -56,10 +69,7 @@ export function RecipeForm({ recipe }: { recipe?: Recipe }) {
           </div>
         </div>
         <div>
-          <button
-            type="button"
-            className="ml-5 mt-3 btn btn-primary bg-primary text-black rounded-md!"
-          >
+          <button className="ml-5 mt-3 btn btn-primary bg-primary text-black rounded-md!">
             Enviar
           </button>
         </div>

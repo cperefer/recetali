63b6a9ff16bdfa15416303c3825105d53bb57387
Miguel <migui.kaos14@gmail.com>
2026-07-14 11:52:57 +0200

    fix: added fix of dir naming

--- src/app/(recipe)/recipe/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function RecipeLayout({ children }: PropsWithChildren) {
-  return (
-    <div className="flex justify-center w-screen min-h-screen">{children}</div>
-  );
-}

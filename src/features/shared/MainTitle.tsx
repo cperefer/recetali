15f014829e8a1b9edf15de26c4098b76ed2c32da
Miguel <migui.kaos14@gmail.com>
2026-07-15 13:27:27 +0200

    feat: added MainTitle styles and icon

--- src/features/shared/MainTitle.tsx
+++ src/features/shared/MainTitle.tsx
@@ -2,9 +2,19 @@ import { ChefHat } from "lucide-react";
 
 export default function MainTitle() {
   return (
-    <div>
-      <ChefHat />
-      <p>RecetAli</p>
+    <div className="w-2/3 md:w-4/12 h-10 flex flex-row">
+      <div className="h-4/5 max-h-12 min-h-7 pr-2">
+        <p>
+          <span>
+            <ChefHat size={30} />
+          </span>
+        </p>
+      </div>
+      <div>
+        <p className="text-2xl font-bold">
+          Recet<span className="text-primary">Ali</span>
+        </p>
+      </div>
     </div>
   );
 }

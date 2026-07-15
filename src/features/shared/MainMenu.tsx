1732550ed82c73600dc515818badbb0ca3353aa7
Miguel <migui.kaos14@gmail.com>
2026-07-15 13:44:15 +0200

    feat: added class for active link

--- src/features/shared/MainMenu.tsx
+++ src/features/shared/MainMenu.tsx
@@ -1,29 +1,53 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MainMenu({
   isAuthenticated,
 }: {
   isAuthenticated: boolean;
 }) {
+  const pathname = usePathname();
+
+  const activeLinkClass = "border-b pb-2 border-primary";
+  const getLinkClass = (href: string) =>
+    pathname === href ? activeLinkClass : "";
+
   return (
-    <div>
-      <ul>
+    <div className="hidden md:block w-2/4">
+      <ul className="flex flex-row gap-2">
         <li>
-          <Link href={"/"}>Inicio</Link>
+          <Link href={"/"} className={getLinkClass("/")}>
+            Inicio
+          </Link>
         </li>
         <li>
-          <Link href={"/recipes"}>Recetas</Link>
+          <Link href={"/recipes"} className={getLinkClass("/recipes")}>
+            Recetas
+          </Link>
         </li>
         <li>
-          <Link href={"/"}>Categorías</Link>
+          <Link href={"/categories"} className={getLinkClass("/categories")}>
+            Categorías
+          </Link>
         </li>
         {isAuthenticated && (
           <>
             <li>
-              <Link href="/recipes/add">Mis recetas</Link>
+              <Link
+                href="/recipes/add"
+                className={getLinkClass("/recipes/add")}
+              >
+                Mis recetas
+              </Link>
             </li>
             <li>
-              <Link href="/recipes/sopas_de_ajo">Favoritos</Link>
+              <Link
+                href="/recipes/sopas_de_ajo"
+                className={getLinkClass("/recipes/sopas_de_ajo")}
+              >
+                Favoritos
+              </Link>
             </li>
           </>
         )}

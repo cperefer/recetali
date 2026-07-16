20f8e779e9268f5b594e6eeaca099fcaf0c4815e
Miguel <migui.kaos14@gmail.com>
2026-07-16 12:59:55 +0200

    feat: added styles to fit button to right

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -7,7 +7,7 @@ export default async function HomeLayout({ children }: PropsWithChildren) {
 
   return (
     <div className="p-3 flex flex-col items-center w-full min-w-sm h-screen">
-      <div className="w-[90%]">
+      <div className="w-[98%] md:w-[90%] border border-gray-400/30 rounded-2xl">
         <MainHeader session={session} />
         {children}
       </div>
--- src/features/shared/MainHeader.tsx
+++ src/features/shared/MainHeader.tsx
@@ -5,10 +5,12 @@ import MainSession from "./MainSession";
 
 export default function MainHeader({ session }: { session: Session | null }) {
   return (
-    <div className="flex">
+    <div className="px-2 pt-2 flex border-bottom-gray">
       <MainTitle />
       <MainMenu isAuthenticated={!!session} />
-      <MainSession session={session} />
+      <div className="ml-auto">
+        <MainSession session={session} />
+      </div>
     </div>
   );
 }
